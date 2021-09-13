f93d5802d23b2176479732c205796d2918c2e9a6
AlexndrG <alexndr1@mail.ru>
2021-09-13 17:37:35 +0300

    24 - React кабзда как подробно: useEffect, cleanup (сброс, зачистка)

--- src/stories/___useEffect.stories.tsx
+++ src/stories/___useEffect.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {log} from 'util';
 
 
 export default {
@@ -51,17 +52,19 @@ export const SimpleExample = () => {
 }
 
 
-export const SetTimeOutExample = () => {
+export const SetTimeoutExample = () => {
     console.log('setTimeOutExample')
 
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
     useEffect(() => {
-        setTimeout(() => {
+        const timeoutId = setTimeout(() => {
             console.log('setTimeout')
             document.title = counter.toString()
         }, 1000)
+
+        return () => clearTimeout(timeoutId)
     }, [counter])
 
     // setTimeout(() => {
@@ -87,11 +90,13 @@ export const SetIntervalExample = () => {
     const [counter, setCounter] = useState(1)
 
     useEffect(() => {
-        setInterval(() => {
+        const intervalId = setInterval(() => {
             console.log('setInterval tick: ' + counter)
             // setCounter(counter+1)
             setCounter(state => state + 1)
         }, 1000)
+
+        return () => clearInterval(intervalId)
     }, [])
 
     return (
@@ -104,10 +109,9 @@ export const SetIntervalExample = () => {
 }
 
 
-
 const value = (n: number) => {
-    if (n<10) {
-        return '0'+n
+    if (n < 10) {
+        return '0' + n
     } else {
         return n
     }
@@ -117,15 +121,92 @@ export const Clock = () => {
     const [date, setDate] = useState(new Date())
 
     useEffect(() => {
-        setInterval(() => {
+        const id = setInterval(() => {
             console.log(date.toString())
             setDate(new Date())
         }, 1000)
+
+        return () => clearInterval(id)
     }, [])
 
     return (
         <>
             current Time: {`${value(date.getHours())}:${value(date.getMinutes())}:${value(date.getSeconds())}`}
+        </>
+    )
+}
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(0)
+
+    console.log('Component rendered with ' + counter)
+
+    useEffect(() => {
+        console.log('Effect occured ' + counter)
+
+        return () => console.log('RESET EFFECT ' + counter)
+    }, [counter])
+
+    const increase = () => setCounter(counter + 1)
+
+    return (
+        <>
+            Hello, counter: {counter}
+            <button onClick={increase}>+</button>
+        </>
+    )
+}
+
+
+export const KeyTrackerExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('Component rendered with ' + text)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+// 1
+//          setText(state => state + e.key)
+
+// 2
+            setText(text + e.key)
+        }
+
+        window.addEventListener('keypress', handler)
+
+        return () => window.removeEventListener('keypress', handler)
+// 1
+//  }, [])
+
+// 2
+    }, [text])
+
+    return (
+        <>
+            Typed text: {text}
+        </>
+    )
+}
+
+
+export const SetTimeout2Example = () => {
+    const [text, setText] = useState('')
+
+    console.log('Component rendered with ' + text)
+
+    useEffect(() => {
+        const timeoutId = setTimeout(()=>{
+            console.log('TIMEOUT EXPIRED')
+            setText('3 seconds passed')
+        },3000)
+
+        return () => clearInterval(timeoutId)
+    }, [text])
+
+    return (
+        <>
+            Typed text: {text}
         </>
     )
 }
